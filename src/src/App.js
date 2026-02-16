c025a16e463790f385c907270091b2b916c3c446
faisesuplementos-lgtm <faisesuplementos@gmail.com>
2026-02-16 18:20:11 -0300

    Create App.js
    Adicionar App.js

---
+++ src/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Compra+ funcionando!</h1>
+      <p>Seu aplicativo está pronto para rodar na Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
